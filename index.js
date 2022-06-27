bb5c573a1d25fe9db5a362bf3573a3af9e489a16
iamthequeen <88898113+iamthequeen@users.noreply.github.com>
2022-06-26 22:10:41 -0500

    moved images

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const path = require("path");
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.get("/signup", (req, res) => {
+  
+})
+// app.use(express.static(path.join(__dirname, '/public')))
 
-app.use(express.static(path.join(__dirname, '/public')))
-
+app.use(express.static('public'))
 // app.get('/', (req, res) => {
 //     res.send(path.join(__dirname, '/public', 'index.html'))
 //   })
@@ -14,5 +17,4 @@ app.use(express.static(path.join(__dirname, '/public')))
 app.listen(5000, () => {
     console.log('listening to port 5000')
   })
-// app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
